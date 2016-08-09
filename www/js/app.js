8b1a9273d365588086f50ac824dd2977a5f15629
José Deodoro de Oliveira Filho <deodoro.filho@gmail.com>
2016-08-08 22:24:37 -1000

    Find trainee working

--- www/js/app.js
+++ www/js/app.js
@@ -107,6 +107,15 @@ starter.config(function($stateProvider, $urlRouterProvider) {
           controller: "PatrolCtrl"
         }
       }
+    })
+    .state('app.find_trainee', {
+      url: "/find_trainee",
+      views: {
+        'equipment-tab': {
+          templateUrl: "templates/find_trainee.html",
+          controller: "FindTraineeCtrl"
+        }
+      }
     });
 
   $urlRouterProvider.otherwise("/app/equipment");
@@ -121,4 +130,71 @@ starter.config(function($stateProvider, $urlRouterProvider) {
       StatusBar.styleDefault();
     }
   });
+})
+.directive('searchBar', [function () {
+  return {
+    scope: {
+      ngModel: '='
+    },
+    require: ['^ionNavBar', '?ngModel'],
+    restrict: 'E',
+    replace: true,
+    template: '<ion-nav-buttons side="right">'+
+            '<div class="searchBar">'+
+              '<div class="searchTxt" ng-show="ngModel.show">'+
+                  '<div class="bgdiv"></div>'+
+                  '<div class="bgtxt">'+
+                    '<input type="text" placeholder="Procurar..." ng-model="ngModel.txt">'+
+                  '</div>'+
+                '</div>'+
+                '<i class="icon placeholder-icon" ng-click="ngModel.txt=\'\';ngModel.show=!ngModel.show"></i>'+
+            '</div>'+
+          '</ion-nav-buttons>',
+    
+    compile: function (element, attrs) {
+      var icon=attrs.icon
+          || (ionic.Platform.isAndroid() && 'ion-android-search')
+          || (ionic.Platform.isIOS()     && 'ion-ios7-search')
+          || 'ion-search';
+      angular.element(element[0].querySelector('.icon')).addClass(icon);
+      
+      return function($scope, $element, $attrs, ctrls) {
+        var navBarCtrl = ctrls[0];
+        $scope.navElement = $attrs.side === 'right' ? navBarCtrl.rightButtonsElement : navBarCtrl.leftButtonsElement;
+        
+      };
+    },
+    controller: ['$scope','$ionicNavBarDelegate', function($scope,$ionicNavBarDelegate){
+      var title, definedClass;
+      $scope.$watch('ngModel.show', function(showing, oldVal, scope) {
+        if(showing!==oldVal) {
+          if(showing) {
+            if(!definedClass) {
+              var numicons=$scope.navElement.children().length;
+              angular.element($scope.navElement[0].querySelector('.searchBar')).addClass('numicons'+numicons);
+            }
+            
+            title = $ionicNavBarDelegate.getTitle();
+            $ionicNavBarDelegate.setTitle('');
+          } else {
+            $ionicNavBarDelegate.setTitle(title);
+          }
+        } else if (!title) {
+          title = $ionicNavBarDelegate.getTitle();
+        }
+      });
+    }]
+  };
+}])
+.factory('TraineeInfo', function () {
+    var _trainee = null;
+ 
+    return {
+        set: function (trainee) {
+            _trainee = trainee;
+        },
+        get: function () {
+            return _trainee;
+        }
+    };
 });
--- www/js/equipment.js
+++ www/js/equipment.js
@@ -1,9 +1,31 @@
-starter.controller("CheckoutCtrl", function($scope, $http) {
+starter.controller("CheckoutCtrl", function($scope, $http, TraineeInfo) {
+    $scope.checkout = {
+        'trainee': null,
+        'equipment': [],
+        'notes': null
+    };
+    TraineeInfo.set(null);
     console.log("Checkout");
+    $scope.$on("selectedTrainee", function() {
+        console.log("selected");
+        // $scope.checkout.trainee = trainee;
+    });
+    $scope.$on("$ionicView.enter", function(){
+      if ($scope.checkout.trainee != TraineeInfo.get())
+        $scope.checkout.trainee = TraineeInfo.get();
+    });
 }).controller("CheckinCtrl", function($scope, $http) {
     console.log("Checkin");
 }).controller("LookupCtrl", function($scope, $http) {
     console.log("Lookup");
+}).controller("FindTraineeCtrl", function($scope, $rootScope, TraineeInfo) {
+    $scope.trainees = [{ "id": 0, "fullname": "A 1" }, { "id": 1, "fullname": "A 2" }, { "id": 2, "fullname": "A 3" }];
+
+    $scope.clickTrainee = function(trainee) {
+        TraineeInfo.set(trainee);
+        $rootScope.$broadcast("selectedTrainee", trainee);
+        $rootScope.$ionicGoBack();
+    };
 });
 
 
